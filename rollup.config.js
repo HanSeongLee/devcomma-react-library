ebfb0d390b8a3c9aee0ba816e2bb435173a3a0ff
HanSeongLee <jkhanseong@gmail.com>
2024-02-24 15:31:46 +0900

    chore: install rollup-plugin-postcss

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
@@ -29,6 +30,7 @@ export default [
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
             terser(),
+            postcss(),
         ],
         external: ["react", "react-dom"],
     },
@@ -36,5 +38,6 @@ export default [
         input: "src/index.ts",
         output: [{ file: "dist/types.d.ts", format: "es" }],
         plugins: [dts.default()],
+        external: [/\.css$/],
     },
 ];
